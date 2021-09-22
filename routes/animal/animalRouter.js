03404947295fac837c4074ba939ce279bca5b1cc
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-22 12:03:07 -0400

    .

--- routes/animal/animalRouter.js
+++ routes/animal/animalRouter.js
@@ -4,7 +4,16 @@ var animalController = require("./controller/animalController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.json({ message: "animal page" });
+  // res.json({ message: "animal page" });
+  animalController.getAllAnimal({}, function(err, foundAnimal){
+    if (err){
+      res
+        .status(500)
+        .json({message: "Something went wrong!", error: err.message})
+    } else {
+      res.json({message: "Success!", foundAnimal})
+    }
+  })
 });
 
 router.post("/create-animal", function (req, res) {
