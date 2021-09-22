43c6288d1be9ad616d2e35686fd4cae9bb4d345c
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-22 10:04:38 -0400

    adding Paks notes

--- routes/animal/animalRouter.js
+++ routes/animal/animalRouter.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var animalController = require("./controller/animalController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'animal page' });
+router.get("/", function (req, res, next) {
+  res.json({ message: "animal page" });
+});
+
+router.post("/create-animal", function (req, res) {
+  //the naming convention is called payload
+  animalController.createAnimal(req.body, function (err, savedAnimal) {
+    if (err) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong!", error: err.message });
+    } else {
+      res.json({ message: "success", savedAnimal });
+    }
+  });
 });
 
 module.exports = router;
